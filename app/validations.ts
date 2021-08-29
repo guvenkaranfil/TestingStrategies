2e3e69a6f42d0fc4540d71b1e226e7c21324f871
guvenkaranfil <guvenkaranfil235@gmail.com>
2021-08-29 13:48:31 +0300

    fix: return boolean for email validation

--- app/validations.ts
+++ app/validations.ts
@@ -1,7 +1,7 @@
 function isEmailValid(email: string): Boolean {
   const re =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  re.test(String(email).toLowerCase());
+  return re.test(String(email).toLowerCase());
 }
 
 export {isEmailValid};
