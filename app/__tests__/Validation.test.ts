ca704f211e8a66ac0224737ab9933eeec1199db5
guvenkaranfil <guvenkaranfil235@gmail.com>
2021-08-29 13:54:22 +0300

    feat: should email validation return true for valid email

--- app/__tests__/Validation.test.ts
+++ app/__tests__/Validation.test.ts
@@ -4,4 +4,8 @@ describe('Validations', () => {
   test('should email validation return false for unvalid email', () => {
     expect(isEmailValid('email.com')).toBe(false);
   });
+
+  test('should email validation return true for valid email', () => {
+    expect(isEmailValid('email@email.com')).toBe(true);
+  });
 });
