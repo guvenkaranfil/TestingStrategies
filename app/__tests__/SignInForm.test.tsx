8a7d69065ec663bee9643ed862267d3fdae393a5
guvenkaranfil <guvenkaranfil235@gmail.com>
2021-08-29 15:21:57 +0300

    refactor: extract form text values into variable to simplify test setup.

--- app/__tests__/SignInForm.test.tsx
+++ app/__tests__/SignInForm.test.tsx
@@ -7,37 +7,40 @@ import {
 
 import SignInForm from '../SignInForm';
 
+const EMAIL_INPUT = /e-mail/i;
+const PASSWORD_INPUT = /şifre/i;
+const SIGNIN_BUTTON = /giriş yap/i;
+const WELCOME_MESSAGE = /hoşgeldin!/i;
+
 describe('SignInForm Component', () => {
   test('should signIn with valid user credentials', async () => {
-    const signInButton = /giriş yap/i;
     const {getByPlaceholderText, getByText} = render(<SignInForm />);
 
-    fireEvent.changeText(getByPlaceholderText(/e-mail/i), 'email@email.com');
-    fireEvent.changeText(getByPlaceholderText(/şifre/i), '1234');
-    fireEvent.press(getByText(signInButton));
-    await waitForElementToBeRemoved(() => getByText(signInButton));
+    fireEvent.changeText(getByPlaceholderText(EMAIL_INPUT), 'email@email.com');
+    fireEvent.changeText(getByPlaceholderText(PASSWORD_INPUT), '1234');
+    fireEvent.press(getByText(SIGNIN_BUTTON));
+    await waitForElementToBeRemoved(() => getByText(SIGNIN_BUTTON));
 
-    expect(getByText(/hoşgeldin!/i)).toBeTruthy();
+    expect(getByText(WELCOME_MESSAGE)).toBeTruthy();
   });
 
   test('should show error message with invalid user credentials', async () => {
-    const signInButton = /giriş yap/i;
     const errorMessage = /bir sorun oluştu!/i;
     const {getByPlaceholderText, getByText} = render(<SignInForm />);
 
-    fireEvent.changeText(getByPlaceholderText(/e-mail/i), 'email@email.com');
-    fireEvent.changeText(getByPlaceholderText(/şifre/i), '');
-    fireEvent.press(getByText(signInButton));
+    fireEvent.changeText(getByPlaceholderText(EMAIL_INPUT), 'email@email.com');
+    fireEvent.changeText(getByPlaceholderText(PASSWORD_INPUT), '');
+    fireEvent.press(getByText(SIGNIN_BUTTON));
 
     expect(getByText(errorMessage)).toBeTruthy();
 
-    fireEvent.changeText(getByPlaceholderText(/e-mail/i), '');
-    fireEvent.changeText(getByPlaceholderText(/şifre/i), '1234');
+    fireEvent.changeText(getByPlaceholderText(EMAIL_INPUT), '');
+    fireEvent.changeText(getByPlaceholderText(PASSWORD_INPUT), '1234');
 
     expect(getByText(errorMessage)).toBeTruthy();
 
-    fireEvent.changeText(getByPlaceholderText(/e-mail/i), '');
-    fireEvent.changeText(getByPlaceholderText(/şifre/i), '');
+    fireEvent.changeText(getByPlaceholderText(EMAIL_INPUT), '');
+    fireEvent.changeText(getByPlaceholderText(PASSWORD_INPUT), '');
 
     expect(getByText(errorMessage)).toBeTruthy();
   });
